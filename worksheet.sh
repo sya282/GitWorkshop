90b1a0ba39fdb071de07b647b6a9d05345a3c0f7
sya282 <66133915+sya282@users.noreply.github.com>
2020-10-05 19:56:36 -0400

    Yoon Ah Shin SESYNC

---
+++ worksheet.sh
@@ -0,0 +1,12 @@
+# Configure git
+
+# This line is only needed if you are not using a SESYNC server
+git config --global init.defaultBranch main 
+
+
+git config --global user.name ...
+git config --global user.email ...
+
+# Link your local repository to the origin repository on GitHub, by
+# copying the code shown on your GitHub repo under the heading:
+# "…or push an existing repository from the command line"
